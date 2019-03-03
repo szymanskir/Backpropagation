036c09f23c2a2681348856fa31b9750214e96ac9
szymanskir <ryszard.szymanski@outlook.com>
2019-03-03 14:53:39 +0100

    Create initial project structure:
    * prepare cookiecutter structure
    * add sample test
    * setup Travis CI

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='backpropagation',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Backpropagation implemepentation for  Deep Learning classes at the Warsaw University of Technology',
+    author='Paweł Rzepiński, Ryszard Szymański',
+    license='MIT',
+)
---
+++ tests/test_sample.py
@@ -0,0 +1,4 @@
+import pytest
+
+def test_sample():
+    assert 2+3 == 5
