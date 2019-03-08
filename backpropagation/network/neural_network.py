2675e92f5fbd457b0244146a2ad2be8c986776d7
szymanskir <ryszard.szymanski@outlook.com>
2019-03-08 18:00:13 +0100

    Switch to row notation in numpy

--- backpropagation/network/neural_network.py
+++ backpropagation/network/neural_network.py
@@ -1,52 +1,106 @@
-from typing import List, Tuple
+import random
 import numpy as np
 
+from typing import List, Tuple
 from .activation_function import IActivationFunction
 from .cost_function import ICostFunction
 
 
 class NeuralNetwork():
-    def __init__(self,
-                 neurons_count_per_layer: List[int],
-                 activation_function: IActivationFunction,
-                 cost_function: ICostFunction,
-                 random_seed: int = None):
+    """ Feedforward neural network using SGD for training.
+
+        Attributes:
+            layers_count (int): Number of layers in the network.
+
+            activation_function (IActivationFunction):
+                Function used for calculating the neurons
+                activation in each layer.
+
+            cost_function (ICostFunction):
+                Function used for calculating the cost of the neural network.
+
+            weights (np.array):
+                Neural network weight values.
+
+            biases (np.array):
+                Neural network biases values.
+    """
+    def __init__(
+            self,
+            neurons_count_per_layer: List[int],
+            activation_function: IActivationFunction,
+            cost_function: ICostFunction,
+            random_seed: int = None
+    ):
+
         if random_seed is not None:
             np.random.seed(random_seed)
 
         self.layers_count = len(neurons_count_per_layer)
-        self.weights = [
-            np.random.normal(
-                size=(neurons_count_per_layer[layer_number],
-                      neurons_count_per_layer[layer_number + 1]))
-            for layer_number in range(0, self.layers_count - 1)
-        ]
-        self.biases = [
-            np.random.normal(size=(neurons_count_per_layer[layer_number], 1))
-            for layer_number in range(0, self.layers_count - 1)
-        ]
+
+        self.weights = np.array([
+            np.random.normal((
+                neurons_count_per_layer[layer],
+                neurons_count_per_layer[layer + 1]
+            ))
+            for layer in range(self.layers_count - 1)
+        ])
+
+        self.biases = np.array([
+            np.random.normal((neurons_count_per_layer[layer], 1))
+            for layer in range(self.layers_count - 1)
+        ])
+
         self.activation_function = activation_function
         self.cost_function = cost_function
 
-    def _feedforward(self, x: np.array):
+    def _feedforward(self, x: np.array) -> np.array:
+        """Calculates the output of the Neural network for the given input.
+
+            Args:
+                x (np.array): Value of the input.
+
+            Returns (np. array):
+                The output values of the neural network for the given input.
+
+        """
         for weight, bias in zip(self.weights, self.biases):
             x = self.activation_function.calculate_value(
                 np.dot(weight, x) + bias)
 
         return x
 
-    def get_cost_function_value(self, X: np.array, y: np.array):
-        activation_function_values = [
+    def get_cost_function_value(self, X: List[np.array], y: List[np.array]):
+        """Calculates the cost value of the neural network for the given inputs.
+
+            Args:
+                X (List[np.array]): List of inputs.
+                y (List[np.array]): List of expected outputs.
+
+            Returns (int):
+                The cost value of the neural network for the given inputs.
+        """
+        activation_values = np.concatenate([
             self._feedforward(observation) for observation in X
-        ]
-        activation_function_values = np.concatenate(activation_function_values)
-        return self.cost_function.calculate_value(y,
-                                                  activation_function_values)
+        ])
+        return self.cost_function.calculate_value(y, activation_values)
 
     def _collect_activation_function_arguments_and_values(
-            self, x: np.array) -> Tuple[np.array, np.array]:
-        activation_function_arguments = list()
-        activation_function_values = [x]
+            self,
+            x: np.array
+    ) -> Tuple[np.array, np.array]:
+        """Collects the activation function arguments and values for each layer.
+
+            Args:
+                x (np.array):
+                    Input of the neural network.
+
+            Returns (Tuple[np.array, np.array]):
+                Activation function arguments and values for the given
+                input for each layer.
+        """
+        activation_arguments = list()
+        activation_values = [x]
 
         current_activation_value = x
         for weight, bias in zip(self.weights, self.biases):
@@ -54,12 +108,26 @@ class NeuralNetwork():
                 weight, current_activation_value) + bias
             current_activation_value = self.activation_function.calculate_value(
                 current_activation_argument)
-            activation_function_arguments.append(current_activation_argument)
-            activation_function_values.append(current_activation_value)
+            activation_arguments.append(current_activation_argument)
+            activation_values.append(current_activation_value)
 
-        return activation_function_arguments, activation_function_values
+        return np.array(activation_arguments), np.array(activation_values)
 
-    def _backpropagation(self, x: np.array, y: np.array):
+    def _backpropagation(
+            self,
+            x: np.array,
+            y: np.array
+    ) -> Tuple[np.array, np.array]:
+        """Performs backpropagation and calculates gradient for weights and biases.
+
+            Args:
+                x (np.array): Neural network input.
+                y (np. array): Expected neural network output.
+
+            Returns (Tuple[np.array, np.array]):
+                Tuple compoes of the gradient for weights and gradient
+                for biases.
+        """
         activation_function_arguments, activation_function_values = (
             self._collect_activation_function_arguments_and_values(x))
 
@@ -87,15 +155,30 @@ class NeuralNetwork():
         weights_gradient = np.zeros(self.weights.shape)
         for layer in range(1, self.layers_count):
             weights_gradient[-layer] = np.dot(
-                activation_function_values[-layer - 1],
-                np.transpose(errors[-layer])
+                np.atleast_2d(activation_function_values[-layer - 1]).transpose(),
+                np.atleast_2d(errors[-layer])
             )
             biases_gradient[-layer] = errors[-layer]
 
         return weights_gradient, biases_gradient
 
-    def _gradient_descent(self, samples: List[np.array],
-                          labels: List[np.array], learning_rate: float):
+    def _gradient_descent(
+            self,
+            samples: List[np.array],
+            labels: List[np.array],
+            learning_rate: float
+    ):
+        """Performs gradient descent and updates network weights and biases.
+
+           The gradient is calculated using the backpropagation method.
+
+        Args:
+            samples (List[np.array]):
+                List of inputs used for minimizing the cost function.
+
+            labels (List[np.array]):
+                List of expected outputs used for minimizing the cost function.
+        """
 
         weight_gradient = [np.zeros(weight.shape) for weight in self.weights]
         biases_gradient = [np.zeros(bias.shape) for bias in self.biases]
@@ -109,3 +192,40 @@ class NeuralNetwork():
             self.weights, (learning_rate / len(samples)) * weight_gradient)
         self.biases = np.subtract(
             self.biases, (learning_rate / len(samples)) * biases_gradient)
+
+    def _stochastic_gradient_descent(
+            self,
+            samples: np.array,
+            labels: np.array,
+            learning_rate: float = 0.5,
+            mini_batch_size: int = 10,
+            epocs_count: int = 10
+    ):
+        """Optimizes the neural network weights and biases using SGD.
+
+            Args:
+                samples (List[np.array]):
+                    List of inputs used for neural network training.
+
+                labels (List[np.array]):
+                    List of expected outputs used for neural network training.
+
+                learning_rate (float):
+                    Learning rate coefficient.
+
+                mini_batch_size (int):
+                    Size of mini batches used during gradient descent.
+
+                epocs_count (int):
+                    Number of epocs used during training.
+        """
+        training_data = list(zip(samples, labels))
+        mini_batch_count = len(training_data)//mini_batch_size
+        for epoc in range(epocs_count):
+            np.random.shuffle(training_data)
+            for mini_batch in range(mini_batch_count):
+                start_index = mini_batch*mini_batch_size
+                training_batch = training_data[
+                    start_index:start_index + mini_batch_size]
+                samples, labels = map(list, zip(*training_batch))
+                self._gradient_descent(samples, labels, learning_rate)
--- tests/test-network/test_neural_network.py
+++ tests/test-network/test_neural_network.py
@@ -3,21 +3,17 @@ import numpy as np
 
 from backpropagation.network.neural_network import NeuralNetwork
 from backpropagation.network.activation_function import (
-    IdentityActivationFunction,
-    SigmoidActivationFunction)
+    IdentityActivationFunction, SigmoidActivationFunction)
 from backpropagation.network.cost_function import (MSECostFunction,
                                                    SECostFunction)
-from numpy.testing import assert_array_equal
+from numpy.testing import assert_array_equal, assert_allclose
 
 
 @pytest.mark.parametrize(
     "neurons_count_per_layer, weights, biases, test_input, expected",
-    [([1, 1], [np.array([[1]])], [np.array([[-1]])], np.array([[1]]),
-      np.array([[0.5]])),
-     ([2, 2, 1], [np.array([[1, 1], [2, -1]]),
-                  np.array([[1, -1]])],
-      [np.array([[-3], [0]]), np.array([[0]])], np.array([[1], [2]]),
-      np.array([[0.5]]))])
+    [([1, 1], np.array([1]), np.array([-1]), np.array([1]), np.array([0.5])),
+     ([2, 2, 1], np.array([[[1, 1], [2, -1]], [[1, -1]]]),
+      np.array([[-3, 0], [0]]), np.array([1, 2]), np.array([0.5]))])
 def test_neural_network_feedforward(neurons_count_per_layer, weights, biases,
                                     test_input, expected):
     nn = NeuralNetwork(
@@ -32,9 +28,14 @@ def test_neural_network_feedforward(neurons_count_per_layer, weights, biases,
 
 
 @pytest.mark.parametrize(
-    "neurons_count_per_layer, weights, biases, X_input, y_input, expected",
-    [([1, 1], [np.array([[1]])], [np.array([[-1]])], np.array([[1]]),
-      np.array([[1]]), 0.125)])
+    "neurons_count_per_layer, weights, biases, X_input, y_input, expected", [(
+        [1, 1],
+        np.array([[1], [-1]]),
+        np.array([[-1]]),
+        np.array([1]),
+        np.array([1]),
+        0.125
+    )])
 def test_neural_network_cost_value(neurons_count_per_layer, weights, biases,
                                    X_input, y_input, expected):
     nn = NeuralNetwork(
@@ -53,22 +54,60 @@ def test_neural_network_backpropagation():
     nn = NeuralNetwork(
         neurons_count_per_layer=[2, 2, 2],
         activation_function=IdentityActivationFunction(),
-        cost_function=SECostFunction())
+        cost_function=MSECostFunction())
 
-    nn.weights = np.array([
-        [[1, 1], [2, 2]],
-        [[2, 2], [1, 1]]
-    ])
-    nn.biases = np.array([[[0], [1]], [[1], [0]]])
+    nn.weights = np.array([[[1, 1], [2, 2]], [[2, 2], [1, 1]]])
+    nn.biases = np.array([[0, 1], [1, 0]])
 
-    X = np.array([[2], [3]])
-    y = np.array([[0], [1]])
+    X = np.array([2, 3])
+    y = np.array([0, 1])
 
     weights_gradient, biases_gradient = nn._backpropagation(X, y)
-    assert_array_equal(weights_gradient, np.array([
-        [[162, 162], [243, 243]],
-        [[165, 75], [363, 165]]
-    ]))
-    assert_array_equal(biases_gradient, np.array([
-        [[81], [81]], [[33], [15]]
-    ]))
+    assert_array_equal(
+        weights_gradient,
+        np.array([[[162, 162], [243, 243]], [[165, 75], [363, 165]]]))
+    assert_array_equal(biases_gradient, np.array([[81, 81], [33, 15]]))
+
+
+def test_neural_network_gradient_descent():
+    nn = NeuralNetwork(
+        neurons_count_per_layer=[2, 2, 2],
+        activation_function=IdentityActivationFunction(),
+        cost_function=SECostFunction())
+
+    nn.weights = np.array([[[1, 1], [2, 2]], [[2, 2], [1, 1]]])
+    nn.biases = np.array([[0, 1], [1, 0]])
+
+    X = [np.array([2, 3])]
+    y = [np.array([0, 1])]
+
+    nn._gradient_descent(X, y, 0.5)
+    assert_allclose(
+        nn.weights,
+        np.array([[[-80, -80], [-119.5, -119.5]],
+                  [[-80.5, -35.5], [-180.5, -81.5]]]))
+
+
+def test_neural_network_stochastic_gradient_descent():
+    nn = NeuralNetwork(
+        neurons_count_per_layer=[2, 2, 2],
+        activation_function=SigmoidActivationFunction(),
+        cost_function=MSECostFunction())
+
+    nn.weights = np.array([[[1, 0], [0, 1]], [[1, 0], [0, 1]]])
+
+    X = np.array([[[0.1], [0]], [[0.2], [0.1]], [[0.3], [0]], [[0.11], [0.14]],
+                  [[0.21], [0.20]], [[0.31], [0.50]], [[0.30], [0.30]],
+                  [[0.51], [0.51]], [[0.18], [0.22]], [[0.1], [0]],
+                  [[0.123], [0.2131231]], [[0], [0.1]], [[0], [0.2]],
+                  [[0.123], [0.16]], [[0.12312], [0.1231231]],
+                  [[0.1249124], [0.123912931]]])
+
+    y = np.array([[[0.1], [0]], [[0.2], [0.1]], [[0.3], [0]], [[0.11], [0.14]],
+                  [[0.21], [0.20]], [[0.31], [0.50]], [[0.30], [0.30]],
+                  [[0.51], [0.51]], [[0.18], [0.22]], [[0.1], [0]],
+                  [[0.123], [0.2131231]], [[0], [0.1]], [[0], [0.2]],
+                  [[0.123], [0.16]], [[0.12312], [0.1231231]],
+                  [[0.1249124], [0.123912931]]])
+
+    nn._stochastic_gradient_descent(X, y)
